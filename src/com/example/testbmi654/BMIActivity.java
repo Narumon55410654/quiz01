34c942811c59672e977bdc0ca4965d805c60ff45
narumon55410654 <nut2103_nutty@hotmail.com>
2014-12-17 11:46:02 +0700

    first commit

---
+++ src/com/example/testbmi654/BMIActivity.java
@@ -0,0 +1,16 @@
+package com.example.testbmi654;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class BMIActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
